04deae8fe58938cb70c491b32c3c3f347524801d
Da Xue <da@libre.computer>
2023-08-19 09:54:37 -0400

    genimage: define bootloader offset

--- board/librecomputer/genimage/efi-btrfs/genimage.sh
+++ board/librecomputer/genimage/efi-btrfs/genimage.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-set -x
+set -ex
 
 BOARD=$2
+
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+. $SCRIPT_DIR/../bootloader.sh
+
 GENIMG_CFG=$BINARIES_DIR/genimage.cfg
 
-wget -nc https://boot.libre.computer/ci/$BOARD -O $BINARIES_DIR/$BOARD
+BOOTLOADER_get $BOARD $BINARIES_DIR/$BOARD
 
-cp -f $(dirname "${BASH_SOURCE[0]}")/genimage.cfg "$BINARIES_DIR/"
-cp -f $(dirname "${BASH_SOURCE[0]}")/boot.ini "$BINARIES_DIR/"
+#wget -nc https://boot.libre.computer/ci/$BOARD -O $BINARIES_DIR/$BOARD
 
+cp -f $SCRIPT_DIR/genimage.cfg "$BINARIES_DIR/"
+cp -f $SCRIPT_DIR/boot.ini "$BINARIES_DIR/"
+
+sed -i "s/__BL_OFFSET__/$(BOOTLOADER_getOffset $BOARD)/" "$GENIMG_CFG"
 sed -i "s/__BOARD__/$BOARD/" "$GENIMG_CFG"
 
 support/scripts/genimage.sh -c "$GENIMG_CFG"
